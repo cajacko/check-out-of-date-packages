7cb6cb83fb402e79d8bc136f6250be349432aabc
Charlie Jackson <charlie@charliejackson.com>
2017-02-27 11:27:02 +0000

    Fixed broken module

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
 const npmCheck = require('npm-check');
 const winston = require('winston');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-module.exports = function(cwd) {
+function isAuthor(npmPackage, checkAuthor) {
+  if (!checkAuthor) {
+    return false;
+  }
+
+  if (!npmPackage) {
+    return false;
+  }
+
+  if (!npmPackage.author) {
+    return false;
+  }
+
+  if (npmPackage.author === checkAuthor) {
+    return true;
+  }
+
+  if (typeof npmPackage.author === 'string' && npmPackage.author.includes(checkAuthor)) {
+    return true;
+  }
+
+  if (!npmPackage.author.name) {
+    return false;
+  }
+
+  if (checkAuthor === npmPackage.author.name) {
+    return true;
+  }
+
+  return false;
+}
+
+module.exports = function checkPackages(cwd, author) {
+  const packageJsonPath = `${cwd}package.json`;
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
   return npmCheck({ cwd }).then((currentState) => {
     const npmPackages = currentState.get('packages');
     let needUpdating = false;
 
     npmPackages.forEach((npmPackage) => {
+      const npmPackageConfigPath = `${cwd}node_modules/${npmPackage.moduleName}/package.json`;
+      const npmPackageConfig = JSON.parse(fs.readFileSync(npmPackageConfigPath, 'utf8'));
+
+      if (author && !isAuthor(npmPackageConfig, author)) {
+        return true;
+      }
+
       if (npmPackage.latest !== npmPackage.installed) {
         needUpdating = true;
-        winston.log('info', `${npmName} Package out of date: ${npmPackage.moduleName} ${npmPackage.installed} -> ${npmPackage.latest}`);
+        winston.log('info', `${packageJson.name} Package out of date: ${npmPackage.moduleName} ${npmPackage.installed} -> ${npmPackage.latest}`);
+        return false;
       }
+
+      return true;
     });
 
     if (needUpdating) {
-      return this.prompt([{
+      return inquirer.prompt([{
         type: 'confirm',
         name: 'continue',
         message: 'This generator has packages which are out of date, do you want to continue?',
@@ -30,4 +77,4 @@ module.exports = function(cwd) {
 
     return true;
   });
-}
+};
